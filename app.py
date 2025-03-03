d33ec79adc580686cb66752bd62403040e1f5f83
hariduvva12 <hariduvva12@gmail.com>
2025-03-03 21:58:41 +0530

    Fix port issue

--- app.py
+++ app.py
@@ -214,4 +214,5 @@ def summary(entry_id):
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    app.run(host="0.0.0.0", port=port)
